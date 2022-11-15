0897d7fac86faeb4a7cec0f58754a32727d4af90
Rodolfo Olivieri <rodolfo.olivieri3@gmail.com>
2022-11-15 19:26:42 -0300

    Small improvements
    Signed-off-by: Rodolfo Olivieri <rodolfo.olivieri3@gmail.com>

--- carnage/logger.py
+++ carnage/logger.py
@@ -6,7 +6,7 @@ from rich.logging import RichHandler
 
 def setup_logger_handler(debug: bool) -> None:
     logging.basicConfig(
-        level=logging.INFO,
+        level=logging.NOTSET,
         format="%(asctime)s: %(message)s",  # noqa
         datefmt="[%X]",
         handlers=[RichHandler()],
